2c9f77e844580a57e2c4e86f034b278b93aaca95
Janet Isak <janetisakidane@gmail.com>
2024-04-02 16:35:10 -0500

    added Student fields

--- src/main/java/com/example/ea_rest_project/domain/Student.java
+++ src/main/java/com/example/ea_rest_project/domain/Student.java
@@ -1,4 +1,21 @@
 package com.example.ea_rest_project.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String major;
+    private Double gpa;
+    private List<Course> coursesTaken;
 }
