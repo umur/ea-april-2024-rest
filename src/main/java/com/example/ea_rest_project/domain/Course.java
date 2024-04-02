9793d63cf19340211c91d2d7b32d716b3e103ed5
Janet Isak <janetisakidane@gmail.com>
2024-04-02 16:25:55 -0500

    added Course fields

--- src/main/java/com/example/ea_rest_project/domain/Course.java
+++ src/main/java/com/example/ea_rest_project/domain/Course.java
@@ -1,6 +1,15 @@
 package com.example.ea_rest_project.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
+    private String id;
+    private String name;
+    private String code;
 
 }
