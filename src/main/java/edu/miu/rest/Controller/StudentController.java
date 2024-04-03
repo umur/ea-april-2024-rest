6ae263dad6dddfd10f8d03c506d4b25d421d8009
tanushaayer <tanushastan@gmail.com>
2024-04-02 21:36:19 -0500

    Adds swagger api

--- src/main/java/edu/miu/rest/Controller/StudentController.java
+++ src/main/java/edu/miu/rest/Controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Students")
+@RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
         private final StudentService studentService;
