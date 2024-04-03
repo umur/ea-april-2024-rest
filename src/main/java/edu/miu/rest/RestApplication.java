6555c1890b7578e4d11ede2dadb1550223074c72
tanushaayer <tanushastan@gmail.com>
2024-04-02 21:49:25 -0500

    removes unnecessary comments

--- src/main/java/edu/miu/rest/RestApplication.java
+++ src/main/java/edu/miu/rest/RestApplication.java
@@ -2,25 +2,11 @@ package edu.miu.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Component;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class RestApplication {
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.any())
-//				.paths(PathSelectors.any())
-//				.build();
-//	}
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
 
