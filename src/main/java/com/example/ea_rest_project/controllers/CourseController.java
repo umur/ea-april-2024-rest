fd3af92cff47f65fd3d191142e46e166e5ac70bb
Janet Isak <janetisakidane@gmail.com>
2024-04-04 14:43:44 -0500

    added the swagger dependency and wrote the api meta data

--- src/main/java/com/example/ea_rest_project/controllers/CourseController.java
+++ src/main/java/com/example/ea_rest_project/controllers/CourseController.java
@@ -4,6 +4,7 @@ import com.example.ea_rest_project.domain.Course;
 
 import com.example.ea_rest_project.dto.CourseRequest;
 import com.example.ea_rest_project.services.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,28 +18,33 @@ import java.util.List;
 public class CourseController {
     private final CourseService courseService;
 
+    @Operation(summary = "Gets all courses", description = "This API will fetch all the courses available")
     @GetMapping
     public List<Course> getAllCourses() {
         return courseService.getAllCourses();
     }
 
+    @Operation(summary = "Gets a course by id", description = "This API will fetch a course using id")
     @GetMapping("/{id}")
     public Course getCourseById(@PathVariable int id) {
         return courseService.getCourseById(id);
     }
 
+    @Operation(summary = "Saves a course", description = "This API will save the course")
     @PostMapping
     public String saveCourse(@RequestBody Course course) {
         courseService.saveCourse(course);
         return "Course saved successfully";
     }
 
+    @Operation(summary = "Updates a course", description = "This API will update the course and its attributes")
     @PutMapping("/{id}")
     public String updateCourse(@PathVariable int id, @RequestBody CourseRequest courseRequest) {
         courseService.updateCourse(id, courseRequest);
         return "Course updated successfully";
     }
 
+    @Operation(summary = "Deletes a course", description = "This API will delete a courses by the given id")
     @DeleteMapping("/{id}")
     public String deleteCourse(@PathVariable int id) {
         courseService.deleteCourse(id);
